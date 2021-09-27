9f4c72fb09bc5819d51da6c15250e0b227319151
UCMO\tkang <tkang@ucmo.edu>
2021-09-27 14:46:57 -0500

    no message

---
+++ src/main/java/com/example/commerce/controller/MyController.java
@@ -0,0 +1,50 @@
+package com.example.commerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+    //localhost:8181/spring
+
+    @GetMapping("spring")
+    public String spring(Model model){
+        model.addAttribute("data1", "Hello David!!!");
+        return "spring";
+    }
+
+    //http://localhost:8181/hello
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "Hello Mac!!!");
+        return "hello";
+    }
+
+
+    @GetMapping("helloapi")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+
+        Hello hello = new Hello();
+        hello.setName(name);
+
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+}
